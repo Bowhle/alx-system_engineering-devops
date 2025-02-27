937e3bcb9d76c979b293ff7fcec22d94228de1bb
bowhle <bowhlem@gmail.com>
2025-02-27 21:49:00 +0200

    API advanced executable 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,17 +4,29 @@
 import requests
 
 
-def top_ten(subreddit):
-    """ print top 10 hot post for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agents': 'api/advanced, task1'}
-    params = {"limit": 10}
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    This queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit.
 
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        results = data.get("data")
-        [print(c.get("data").get("title")) for c in results.get("children")]
+    If not a valid subreddit, return None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
+
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
-        print("None")
+        return None
