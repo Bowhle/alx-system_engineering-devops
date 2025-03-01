65541bdee7554c89cbf5aa08c1f5ab4bc2a33bb0
bowhle <bowhlem@gmail.com>
2025-03-01 22:16:55 +0200

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# This script creates a file in /tmp.
+
+
+file { 'dee_alx_with_puppet_lint':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
