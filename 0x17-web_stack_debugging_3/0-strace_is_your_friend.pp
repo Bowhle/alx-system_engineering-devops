a493e6de48a629316c1c14a3d02a5a3de71e8888
bowhle <bowhlem@gmail.com>
2025-03-03 00:43:11 +0200

    Strace is your friend 5

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,12 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`
+# Ensures that the file exists before running the sed command.
+
+file { '/var/www/html/wp-settings.php':
+  ensure => 'file',
+}
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
+  onlyif  => 'test -f /var/www/html/wp-settings.php', # Ensure the file exists before running sed
 }
